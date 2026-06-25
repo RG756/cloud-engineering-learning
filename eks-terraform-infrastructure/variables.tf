f50eb257ced604de72966730e4972240f5e01d69
RG756 <rgoto.lancaster.biel@gmail.com>
2026-06-25 22:32:46 +0900

    Phase 2: Add EC2, EBS with KMS encryption Terraform code

---
+++ eks-terraform-infrastructure/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "eks-terraform"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ebs_volume_size" {
+  description = "EBS volume size in GB"
+  type        = number
+  default     = 20
+}
